257f0e02b1c733e5806ea6248c23e88fd87ade19
irisfield <aquezadawa@gmail.com>
2024-10-20 10:49:46 -0400

    Solution for LC 17

---
+++ techniques/backtracking/letter_combinations.py
@@ -0,0 +1,34 @@
+class Solution:
+    """LeetCode 17: Letter Combinations of a Phone Number"""
+    def letterCombinations(self, digits: str) -> list[str]:
+        res = []
+        digitToChar = {
+          "2": "abc",
+          "3": "def",
+          "4": "ghi",
+          "5": "jkl",
+          "6": "mno",
+          "7": "qprs",
+          "8": "tuv",
+          "9": "wxyz",
+        }
+
+        def backtrack(i: int, curStr: str) -> None:
+            if len(curStr) == len(digits):
+                return res.append(curStr)
+            for c in digitToChar[digits[i]]:
+                # print(f"{i} {curStr} {c}")
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        """
+        The time complexity is O(n * 4 ^ n):
+        - There are n characters in digits.
+        - 7 and 9 are the worst case scenario mapping to 4 characters each.
+        - There are 4 possible combinations for each of the 4 characters.
+        """
+        return res
+
+print(Solution().letterCombinations("23"))
