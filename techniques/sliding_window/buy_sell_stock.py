317adb95e92afc2072ca82301818ead50d7e2a57
irisfield <aquezadawa@gmail.com>
2024-10-21 19:47:35 -0400

    Sliding window solution for LC 121

---
+++ techniques/sliding_window/buy_sell_stock.py
@@ -0,0 +1,20 @@
+class Solution:
+    """121. Best Time to Buy and Sell Stock"""
+    def maxProfit(self, prices: list[int]) -> int:
+        buy, sell = 0, 1
+        maxProfit = 0
+        while sell < len(prices):
+            if prices[buy] < prices[sell]:
+                profit = prices[sell] - prices[buy]
+                maxProfit = max(maxProfit, profit)
+            else:
+                buy = sell
+            sell += 1
+        """
+        Time complexity is O(n), as each element is processed once.
+        Space complexity is O(1), as no data structures were utilized.
+        """
+        return maxProfit
+
+assert Solution().maxProfit([7,6,4,3,1]) == 0, f"Max profit should be 0"
+assert Solution().maxProfit([7,1,5,3,6,4]) == 5, f"Max profit should be 5"
