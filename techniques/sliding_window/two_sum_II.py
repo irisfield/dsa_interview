d6044b7024541acddb6ae5ae251f74549873055f
irisfield <aquezadawa@gmail.com>
2024-10-23 23:42:57 -0400

    Sliding window solution for LC 167

---
+++ techniques/sliding_window/two_sum_II.py
@@ -0,0 +1,18 @@
+class Solution:
+    """167. Two Sum II - Input Array Sorted"""
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        # NOTE: The solution must use constant extra space.
+        l, r = 0, len(nums) - 1
+        while l < r:  # time O(n)
+            total = nums[l] + nums[r]
+            if  total > target:
+                r -= 1
+            elif total < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]  # 1-indexed array
+        """
+        Time complexity is O(n), as each element is processed exactly once.
+        Space complexity is O(1), as no data structures were utilized.
+        """
+        return []  # a solution is guaranteed
