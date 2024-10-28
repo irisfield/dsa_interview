61af1c035123ace390e8d7a9cfde5564493f9bd2
irisfield <aquezadawa@gmail.com>
2024-10-28 00:06:32 -0400

    Elegant solution for LC 150

--- data_structures/stacks/reverse_polish_notation.py
+++ data_structures/stacks/reverse_polish_notation.py
@@ -2,19 +2,18 @@ class Solution:
     """150. Evaluate Reverse Polish Notation"""
     def evalRPN(self, tokens: list[str]) -> int:
         stack = []
+        ops = {
+            "+": lambda a, b: b + a,
+            "-": lambda a, b: b - a,
+            "*": lambda a, b: b * a,
+            "/": lambda a, b: int(b / a)
+        }
+
         for t in tokens:  # time O(n)
-            if t == "+":
-                a, b = stack.pop(), stack.pop()
-                stack.append(b + a)
-            elif t == "-":
-                a, b = stack.pop(), stack.pop()
-                stack.append(b - a)
-            elif t == "*":
-                a, b = stack.pop(), stack.pop()
-                stack.append(b * a)
-            elif t == "/":
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(b / a))
-            else:
+            if t not in ops:
                 stack.append(int(t))  # space O(n)
+            else:
+                res = ops[t](stack.pop(), stack.pop())
+                stack.append(res)
+
         return stack.pop()
