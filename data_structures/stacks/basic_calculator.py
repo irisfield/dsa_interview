f498d9570bcc6522a715d554473727578c07139a
irisfield <aquezadawa@gmail.com>
2024-10-31 11:51:52 -0400

    Solution for LC 224

--- data_structures/stacks/basic_calculator.py
+++ data_structures/stacks/basic_calculator.py
@@ -3,19 +3,25 @@ class Solution:
     def calculate(self, s: str) -> int:
         res, sign, stack = 0, 1, []
 
+        cur = 0
         for c in s:
             if c.isdigit():
-                res += int(c) * sign
+                cur = cur * 10 + int(c)
             elif c in "+-":
+                res += cur * sign
+                cur = 0
                 sign = 1 if c == "+" else -1
             elif c == "(":
                 stack.append(res)
                 stack.append(sign)
                 res, sign = 0, 1
             elif c == ")":
+                res += cur * sign
+                cur = 0
                 res *= stack.pop()  # sign
-                res += stack.pop()  # number
-        return res
+                res += stack.pop()  # res
+        return res + (cur * sign)
 
-print(Solution().calculate("(1+(4+5+2)-3)+(6+8)"))  # 23
+print(Solution().calculate("1-11"))  # -10
 print(Solution().calculate("2147483647"))  # 2147483647
+print(Solution().calculate("(1+(4+5+2)-3)+(6+8)"))  # 23
