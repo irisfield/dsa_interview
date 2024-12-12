48bc1b0d34b94ee0641139c5f8aa64b6c0e2e96f
irisfield <aquezadawa@gmail.com>
2024-12-12 19:03:14 +0900

    Solution for LC 74

---
+++ dsa/matrix/search_a_2d_matrix.py
@@ -0,0 +1,26 @@
+class Solution:
+    """74. Search a 2D Matrix"""
+
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+        """
+        The goal is to remove a row or a column in each interation.
+        """
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        r, c = 0, COLS - 1
+
+        while r < ROWS and c >= 0:
+
+            if target > matrix[r][c]:
+                r += 1
+            elif target < matrix[r][c]:
+                c -= 1
+            else:
+                return True
+        """
+        The time complexity is O(m + n) due to eliminating either a row
+        or a column in each iteration.
+
+        The space complexity is O(1) as no data structures are used.
+        """
+        return False
