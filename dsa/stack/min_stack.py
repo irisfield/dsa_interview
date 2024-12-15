c3d94dbd5dd99d3e7c7fa3004f366255bed62386
irisfield <aquezadawa@gmail.com>
2024-12-15 18:06:38 +0900

    Recategorized solution for LC 155

--- dsa/stack/min_stack.py
+++ dsa/stack/min_stack.py
@@ -1,11 +1,11 @@
 class MinStack:
     """155. Min Stack"""
 
-    def __init__(self):
+    def __init__(self):  # O(n) space
         self.stack = []
         self.min = []  # (idx, val)
 
-    def push(self, val: int) -> None:
+    def push(self, val: int) -> None:  # O(1) time
         idx = len(self.stack)
         if not self.stack:
             self.min.append((idx, val))
@@ -13,23 +13,13 @@ class MinStack:
             self.min.append((idx, val))
         self.stack.append(val)
 
-    def pop(self) -> None:
+    def pop(self) -> None:  # O(1) time
         if self.min and self.min[-1][0] == len(self.stack) - 1:
             self.min.pop()
         return self.stack.pop()
 
-    def top(self) -> int:
+    def top(self) -> int:  # O(1) time
         return self.stack[-1]
 
-    def getMin(self) -> int:
+    def getMin(self) -> int:  # O(1) time
         return self.min[-1][1] if self.min else 0
-
-    # time O(1)
-    # space O(n)
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(val)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
