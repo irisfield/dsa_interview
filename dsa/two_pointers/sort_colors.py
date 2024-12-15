0a898f4e11dc1bb6a692c9a2796a61e061ae7323
irisfield <aquezadawa@gmail.com>
2024-12-15 18:03:51 +0900

    Recategorized solution for LC 75

--- dsa/two_pointers/sort_colors.py
+++ dsa/two_pointers/sort_colors.py
@@ -1,8 +1,10 @@
 class Solution:
     """75. Sort Colors"""
+
     def sortColors(self, nums: list[int]) -> None:
         """Single pass two pointers solution"""
         i, l, r = 0, 0, len(nums) - 1
+
         while i <= r:  # time O(n)
             if nums[i] == 0:
                 nums[l], nums[i] = nums[i], nums[l]
@@ -12,10 +14,19 @@ class Solution:
                 r, i = r - 1, i - 1
             i += 1
 
-    def sortColors2(self, nums: list[int]) -> None:
+        """
+        The time complexity is O(n), as each element is processed once
+        with the two-pointer approach.
+
+        The space complexity is O(1), as no extra space is used other
+        than a few variables.
+        """
+
+    def sortColors(self, nums: list[int]) -> None:
         """Do not return anything, modify nums in-place instead."""
         # Bucket sort - time O(n)
         bucket = {}
+
         for i in range(len(nums)):  # time O(n)
             bucket[nums[i]] = 1 + bucket.get(nums[i], 0)
 
@@ -27,3 +38,12 @@ class Solution:
                 i += 1
             else:
                 j += 1
+
+        """
+        The time complexity is O(n), as each element is processed
+        a constant number of time: once to populate the bucket, once to
+        fill the list with sorted values, and in the while loop.
+
+        The space complexity is O(n), as a bucket is used to store the
+        count of each color in the list.
+        """
