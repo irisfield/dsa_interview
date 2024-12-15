b87fa41c84c9f1e1d2c8028d55bc2d9ae714aff9
irisfield <aquezadawa@gmail.com>
2024-12-15 18:03:51 +0900

    Recategorized solution for LC 56

--- dsa/interval/merge_intervals.py
+++ dsa/interval/merge_intervals.py
@@ -1,5 +1,6 @@
 class Solution:
     """56. Merge Intervals"""
+
     def merge(self, intervals: list[list[int]]) -> list[list[int]]:
         intervals.sort(key=lambda i: i[0])  # time O(n log n)
         output = [intervals[0]]
@@ -11,7 +12,13 @@ class Solution:
                 output[-1][1] = max(prevEnd, end)
             else:
                 output.append([start, end])  # space O(n)
-        return output
 
-print(Solution().merge([[1,4],[0,2],[3,5]]))
-print(Solution().merge([[1,3],[2,6],[8,10],[15,18]]))
+        """
+        The time complexity is O(n log n). The algorithm sorts the
+        intervals, which takes O(n log n), and then processes them in
+        a single pass (O(n)).
+
+        The space complexity is O(n). The space is used to store the
+        output list, which may contain up to n intervals.
+        """
+        return output
