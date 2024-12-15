bb7db85ee0c8f46026c6c49d049db3ecbd882c10
irisfield <aquezadawa@gmail.com>
2024-12-15 18:07:21 +0900

    Recategorized solution for LC 199

--- dsa/tree/binary_tree_right_side_view.py
+++ dsa/tree/binary_tree_right_side_view.py
@@ -9,22 +9,50 @@ from collections import deque
 
 class Solution:
     """199. Binary Tree Right Side View"""
+
     def rightSideView(self, root: Optional[TreeNode]) -> list[int]:
         # Breadth-First Search / Level Ordered Traversal
         res = []
         q = deque([root])
 
         while q:
-            length = len(q)
-            for i in range(length):
+            right = None
+
+            for _ in range(len(q)):
                 node = q.popleft()
-                if i == (length - 1):
-                    res.append(node.val)
+                right = node
+
+                # The order is important: left before right
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
 
+            if right:
+                q.append(right.val)
+
+        """
+        The time complexity is O(n). Each node is processed exactly once.
+        The space complexity is O(w), where w is maximum width of the tree.
+        """
+        return res
+
+
+    def rightSideView(self, root: Optional[TreeNode]) -> list[int]:
+        res = []
+
+        def dfs(node, depth):
+            if not node:
+                return
+
+            if depth == len(res):
+                res.append(node.val)
+
+            # The order is important: right before left
+            dfs(node.right, depth + 1)
+            dfs(node.left, depth + 1)
+
+        dfs(root, 0)
         """
         The time complexity is O(n). Each node is processed exactly once.
         The space complexity is O(w), where w is maximum width of the tree.
