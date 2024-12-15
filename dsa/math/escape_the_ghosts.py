3f34001d0d4db60552568a9b7b4e6a0a956eb067
irisfield <aquezadawa@gmail.com>
2024-12-15 18:03:50 +0900

    Recategorized solution for LC 789

--- dsa/math/escape_the_ghosts.py
+++ dsa/math/escape_the_ghosts.py
@@ -1,5 +1,6 @@
 class Solution:
     """789. Escape The Ghosts"""
+
     def escapeGhosts(self, ghosts: list[list[int]], target: list[int]) -> bool:
         # If the ghost is closer to the target, you cannot escape.
         # If you and the ghost arrive at the same time, you cannot escape.
@@ -10,4 +11,12 @@ class Solution:
             ghost_dist = abs(tx - gx) + abs(ty - gy)
             if ghost_dist <= my_dist:
                 return False  # ghost is closer to the target
+
+        """
+        The time complexity is O(n) because the algorithm loops through
+        all the ghosts to calculate their distance to the target.
+
+        The space complexity is O(1) as no extra space is used other
+        than a few variables for calculations.
+        """
         return True
