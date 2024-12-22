f5cec22c8b25ea3999a85599f012adeb3f0d34a1
irisfield <aquezadawa@gmail.com>
2024-12-22 10:37:26 +0900

    Fixed explanation for LC 133

--- dsa/graph/clone_graph.py
+++ dsa/graph/clone_graph.py
@@ -6,22 +6,32 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
+from typing import Optional
+
 class Solution:
     """133. Clone Graph"""
 
-    def cloneGraph(self, node: "Node") -> "Node":
-        oldToNew = {}
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        copy = {}
 
         def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
-            copy = Node(node.val)
-            oldToNew[node] = copy
+            if node in copy:
+                return copy[node]
+
+            clone = Node(node.val)
+            copy[node] = clone
+
             for nei in node.neighbors:
-                copy.neighbors.append(dfs(nei))
-            return copy
-    """
-    The time complexity is O(V + E) because each node (V) and its edges (E) are traversed once.
-    The space complexity is O(V) due to the dictionary storing clones and the maximum depth of the recursion stack.
-    """
-    return dfs(node) if node is not None else None
+                clone.neighbors.append(dfs(nei))
+            return clone
+        """
+        The time complexity is O(n), where n is the number of nodes in
+        the graph. Each node is visited exactly once during the DFS
+        traversal, and each edge is processed once.
+
+        The space complexity is O(n). This accounts for the space
+        required by the recursion stack (which can grow up to the depth
+        of the graph) and the hash map used to store the mapping of
+        original nodes to their corresponding cloned nodes.
+        """
+        return dfs(node) if node is not None else None
